e6f4b62d3880b54c44f45c9766a473da44e630e6
sony-jun <warrise@naver.com>
2023-01-03 03:03:45 +0900

    0103

--- Algorithm/8_back_jun_17427_약수의합2.py
+++ Algorithm/8_back_jun_17427_약수의합2.py
@@ -1,10 +1,7 @@
-t = int(input())
-for i in range(t):
+n = int(input())
 
-    n = int(input())
+cnt = 0
 
-    cnt = 0
-
-    for i in range(1, n + 1):
-        cnt += (n // i) * i
-    print(cnt)
+for i in range(1, n + 1):
+    cnt += (n // i) * i
+print(cnt)
