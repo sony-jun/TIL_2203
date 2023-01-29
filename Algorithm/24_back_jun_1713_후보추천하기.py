7eea52ffa548bbef0edea612b23e40d20ba87362
sony-jun <warrise@naver.com>
2023-01-30 00:48:44 +0900

    1.30

--- Algorithm/24_back_jun_1713_후보추천하기.py
+++ Algorithm/24_back_jun_1713_후보추천하기.py
@@ -1,3 +1,20 @@
 n = int(input())
 m = int(input())
 hubo = map(int, input().split())
+
+li = []
+num_li = []
+
+for i in hubo:
+    if i in li:
+        a = li.index(i)
+        num_li[a] += 1
+    else:
+        if len(li) >= n:
+            a = num_li.index(min(num_li))
+            del li[a]
+            del num_li[a]
+        li.append(i)
+        num_li.append(1)
+li.sort()
+print(" ".join(map(str, li)))
