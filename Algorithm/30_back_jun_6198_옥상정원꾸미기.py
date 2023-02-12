69349ec09ec507bff8bcae857292b8ae1c09d4a1
sony-jun <warrise@naver.com>
2023-02-13 00:57:12 +0900

    2.13

--- Algorithm/30_back_jun_6198_옥상정원꾸미기.py
+++ Algorithm/30_back_jun_6198_옥상정원꾸미기.py
@@ -1,3 +1,21 @@
-n = int(input())
-for _ in range(n):
-    h = int(input())
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+stack = []
+answer = 0
+for _ in range(N):
+    tower = int(input())
+    if stack:
+        if stack[-1] <= tower:
+            while stack and stack[-1] <= tower:
+                stack.pop()
+            answer += len(stack)
+            stack.append(tower)
+        else:
+            answer += len(stack)
+            stack.append(tower)
+    else:
+        stack.append(tower)
+print(answer)
