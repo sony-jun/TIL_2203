c3e2f434a989ddbddbfa05248085774bb95d2fd7
sony-jun <warrise@naver.com>
2023-02-18 00:07:56 +0900

    2.18

--- Algorithm/35_back_jun_2146_다리만들기.py
+++ Algorithm/35_back_jun_2146_다리만들기.py
@@ -5,3 +5,8 @@ n = int(input())
 matrix = [list(map(int, (input().split()))) for _ in range(n)]
 
 print(matrix)
+
+
+# 일단, 섬인지 아닌지를 판명부터
+# 그 섬들에 이름표(?)를 붙인다
+# 그 다음에 연결을 해보자
---
+++ Algorithm/test.py
@@ -0,0 +1,45 @@
+from collections import deque
+
+
+def bfs(x, y):
+    diff = [
+        [-1, 0],
+        [0, -1],
+        [0, 1],
+        [1, 0],
+    ]
+
+    q = deque()
+    q.append((x, y))
+    maps[x][y] = 0  # 방문처리
+
+    while q:
+        now = q.popleft()
+
+        for i in range(4):
+            nx = now[0] + diff[i][0]
+            ny = now[1] + diff[i][1]
+            if 0 <= nx < h and 0 <= ny < w:
+                if maps[nx][ny] == 1:  # 방문하지 않았다면
+                    maps[nx][ny] = 0
+                    q.append((nx, ny))
+
+
+while True:
+    w, h = map(int, input().split())
+    maps = []
+    ans = 0
+
+    if w == h == 0:
+        break
+
+    for _ in range(h):
+        maps.append(list(map(int, input().split())))
+
+    for i in range(h):
+        for j in range(w):
+            if maps[i][j] == 1:
+                ans += 1
+                bfs(i, j)
+
+    print(ans)
