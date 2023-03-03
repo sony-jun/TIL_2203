af580a1605017eb6564ddc2a3cf8437d1cb4a9c6
sony-jun <warrise@naver.com>
2023-03-03 16:48:38 +0900

    3.3

--- Algorithm/37_SWEA_2805_농작물수확하기.py
+++ Algorithm/37_SWEA_2805_농작물수확하기.py
@@ -10,11 +10,51 @@
 
 
 t = int(input())
-for i in range(1, t + 1):
+for p in range(1, t + 1):
     n = int(input())
     matrix = [list(map(int, input())) for _ in range(n)]
     cnt = 0
     x = n // 2
     y = n // 2
 
-    print(matrix)
+    for i in range(n):
+        for j in range(x, y + 1):
+            cnt += matrix[i][j]
+
+        if i < n // 2:
+            x -= 1
+            y += 1
+        else:
+            x += 1
+            y -= 1
+
+    print(f"#{p} {cnt}")
+
+
+"""
+
+T = int(input())
+
+for tc in range(1, T+1):
+    N = int(input())
+    a = [list(map(int, input())) for _ in range(N)]
+    ans = 0  # output 변수
+
+    # s: 시작포인트, e: 끝포인트
+    s, e = N // 2, N // 2
+    for i in range(N):
+        for j in range(s, e+1):
+            ans += a[i][j]
+        # 행의 인덱스가 mid 전까지는 s-e 간격 늘리고 mid 이후로는 간격 줄임
+        if i < N // 2:
+            s -= 1
+            e += 1
+        else:
+            s += 1
+            e -= 1
+
+    print("#{} {}".format(tc, ans))
+
+
+
+"""
