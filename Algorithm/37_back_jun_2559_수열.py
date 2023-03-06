3f69f9f37c2afb526d545e7a864f416286b5419e
sony-jun <warrise@naver.com>
2023-03-06 17:17:42 +0900

    3.6

--- Algorithm/37_back_jun_2559_수열.py
+++ Algorithm/37_back_jun_2559_수열.py
@@ -1,12 +1,27 @@
-import sys
+# 문제에 있는 그대로 했더니 시간초과가 나는 문제
+
+# import sys
+
+# input = sys.stdin.readline
+
+# cnt = []
+
+# n, k = map(int, input().split())
+# li = list(map(int, input().split()))
+
+# for i in range(n - k + 1):
+#     cnt.append(sum(li[i : i + k]))
+
+# print(max(cnt))
+
 
 n, k = map(int, input().split())
-a = list(map(int, input().split()))
+li = list(map(int, input().split()))
 
-result = []
-result.append(sum(a[:k]))
+cnt = []
+cnt.append(sum(li[:k]))  # 맨처음 값을 미리 더해준다
 
 for i in range(n - k):
-    result.append(result[i] - a[i] + a[k + i])
-
-print(max(result))
+    cnt.append(cnt[i] - li[i] + li[k + i])  # 한칸 씩 이동하면서 처음값 삭제, 다음값 더하는 식으로 더해준다
+# 윗작업보다 빠른 이유는 리스트를 전부 더하는것과 하나빼고 하나더하는것에 계산 차이가 존재하기 때문에...
+print(max(cnt))
---
+++ Algorithm/40_programmers_43165_타겟넘버.py
@@ -0,0 +1,17 @@
+# 모든 값을 다 미리 계산하는건 너무 커지나...?
+# 20이면 음...안되네
+
+
+def solution(numbers, target):
+    answer = 0
+    leaves = [0]
+    for num in numbers:
+        tmp = []
+        for parent in leaves:
+            tmp.append(parent + num)
+            tmp.append(parent - num)
+        leaves = tmp
+    for leaf in leaves:
+        if leaf == target:
+            answer += 1
+    return answer
