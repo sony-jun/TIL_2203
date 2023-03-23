30937c35cdd434f3dbefa4e5520acfedb3a43044
sony-jun <warrise@naver.com>
2023-03-24 00:14:26 +0900

    3.24

---
+++ Algorithm/57_back_jun_9063_대지.py
@@ -0,0 +1,13 @@
+n = int(input())
+a, b = map(int, input().split())
+if n < 2:
+    print(0)
+    exit()
+minX, minY, maxX, maxY = a, b, a, b
+for i in range(n - 1):
+    a, b = map(int, input().split())
+    minX = min(minX, a)
+    minY = min(minY, b)
+    maxX = max(maxX, a)
+    maxY = max(maxY, b)
+print((maxX - minX) * (maxY - minY))
