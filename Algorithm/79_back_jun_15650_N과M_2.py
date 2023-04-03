5b650da61ce63e04bc080ef460f2869757453e81
sony-jun <warrise@naver.com>
2023-04-04 00:53:49 +0900

    4.4

---
+++ Algorithm/79_back_jun_15650_N과M_2.py
@@ -0,0 +1,40 @@
+# 전 문제인 N과M_1 과 비교해 보자
+n, m = list(map(int, input().split()))
+
+s = []
+
+
+def dfs(start):
+    if len(s) == m:
+        print(" ".join(map(str, s)))
+        return
+
+    for i in range(start, n + 1):
+        if i not in s:
+            s.append(i)
+            dfs(i + 1)
+            s.pop()
+
+
+dfs(1)
+
+
+# 78_back_jun_15649_N과M_1
+# n, m = list(map(int, input().split()))
+
+# s = []
+
+
+# def dfs():
+#     if len(s) == m:
+#         print(" ".join(map(str, s)))
+#         return
+
+#     for i in range(1, n + 1):
+#         if i not in s:
+#             s.append(i)
+#             dfs()
+#             s.pop()
+
+
+# dfs()
