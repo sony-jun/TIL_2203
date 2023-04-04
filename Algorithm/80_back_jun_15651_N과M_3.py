cc4c9551c23e7b4c55fdf3d25d47f1b1207c4b53
sony-jun <warrise@naver.com>
2023-04-05 00:18:54 +0900

    4.5

---
+++ Algorithm/80_back_jun_15651_N과M_3.py
@@ -0,0 +1,19 @@
+# 가장 간단한 형태의 DFS 문제 15649문제에서 if문이 빠진걸 알 수 있다
+
+n, m = map(int, input().split())
+
+s = []
+
+
+def dfs():
+    if len(s) == m:
+        print(" ".join(map(str, s)))
+        return
+
+    for i in range(1, n + 1):
+        s.append(i)
+        dfs()
+        s.pop()
+
+
+dfs()
