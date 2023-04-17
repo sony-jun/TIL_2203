e58c76cf28edf998e6205d924bce7b13fa41b838
sony-jun <warrise@naver.com>
2023-04-18 00:28:12 +0900

    4.18

--- Algorithm/91_back_jun_1932_정수삼각형.py
+++ Algorithm/91_back_jun_1932_정수삼각형.py
@@ -1,15 +1,14 @@
 n = int(input())
-t = []
+d = []
 for i in range(n):
-    t.append(list(map(int, input().split())))
-k = 2
+    d.append(list(map(int, input().split())))
+
 for i in range(1, n):
-    for j in range(k):
+    for j in range(len(d[i])):
         if j == 0:
-            t[i][j] = t[i][j] + t[i - 1][j]
-        elif i == j:
-            t[i][j] = t[i][j] + t[i - 1][j - 1]
+            d[i][j] = d[i][j] + d[i - 1][j]
+        elif j == len(d[i]) - 1:
+            d[i][j] = d[i][j] + d[i - 1][j - 1]
         else:
-            t[i][j] = max(t[i - 1][j - 1], t[i - 1][j]) + t[i][j]
-    k += 1
-print(max(t[n - 1]))
+            d[i][j] = max(d[i - 1][j - 1], d[i - 1][j]) + d[i][j]
+print(max(d[n - 1]))
