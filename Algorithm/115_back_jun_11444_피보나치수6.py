68fd61e13d18f3ab7a3ab024cef07b571affc1f1
sony-jun <warrise@naver.com>
2023-05-26 00:10:18 +0900

    5.26

--- Algorithm/115_back_jun_11444_피보나치수6.py
+++ Algorithm/115_back_jun_11444_피보나치수6.py
@@ -1,1 +1,21 @@
+dp = dict()
 
+
+def fibo(n):
+    if dp.get(n) != None:
+        return dp[n]
+    if n == 0:
+        return 0
+    if n == 1 or n == 2:
+        return 1
+    if n % 2 == 0:
+        dp[n // 2 + 1] = fibo(n // 2 + 1) % 1000000007
+        dp[n // 2 - 1] = fibo(n // 2 - 1) % 1000000007
+        return dp[n // 2 + 1] ** 2 - dp[n // 2 - 1] ** 2
+    else:
+        dp[n // 2 + 1] = fibo(n // 2 + 1) % 1000000007
+        dp[n // 2] = fibo(n // 2) % 1000000007
+        return dp[n // 2 + 1] ** 2 + dp[n // 2] ** 2
+
+
+print(fibo(int(input())) % 1000000007)
