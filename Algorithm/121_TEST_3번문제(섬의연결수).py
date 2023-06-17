7bb1d44a6b20d45130fccaaea4db695164eb8679
sony-jun <warrise@naver.com>
2023-06-18 00:46:30 +0900

    6.18

--- Algorithm/121_TEST_3번문제(섬의연결수).py
+++ Algorithm/121_TEST_3번문제(섬의연결수).py
@@ -5,22 +5,23 @@ n, m, k = map(int, input().split())
 graph = [[] * n for _ in range(n + 1)]
 for _ in range(m):
     a, b = map(int, input().split())
-    graph[a].append[b]
-    graph[b].append[a]
+    graph[a].append(b)
+    graph[b].append(a)
 
-visited = [-1] * (n + 1)
+visited = [0] * (n + 1)
 q = deque()
 q.append(1)
 
 while q:
     cur = q.popleft()
     for i in graph[cur]:
-        if visited[i] != -1:
+        if visited[i] != 0:
             continue
-        q.qppend(i)
         visited[i] = visited[cur] + 1
+        q.append(i)
 
-if 1 <= visited[n] < k:
+
+if 1 <= visited[n] <= k:
     print("YES")
 else:
     print("NO")
--- Algorithm/test.py
+++ Algorithm/test.py
@@ -1,30 +1,41 @@
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-S = input().rstrip()
-Q = []
-Q.append(("", 1))
+N = int(input())
+Y, X = map(int, input().split())
 
-S += "z"
-for c in S:
-    if Q[-1][0] != c:
-        if M <= Q[-1][1]:
-            top = Q[-1][0]
-            while top == Q[-1][0]:
-                Q.pop()
-    if Q[-1][0] == c:
-        Q.append((c, Q[-1][1] + 1))
-        print(Q)
-    else:
-        Q.append((c, 1))
-        print(Q)
-Q.pop()
-print(Q)
+M = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
+V = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
+for i in range(1, N + 1):
+    M[i] = [0] + list(map(int, input().split()))
 
-if len(Q) > 1:
-    for c, n in Q:
-        print(c, end="")
-else:
-    print("CLEAR!")
+K = M[Y][X]
+
+dy = [0, 1, 0, -1]
+dx = [1, 0, -1, 0]
+ans = 0
+
+for i in range(1, N + 1):
+    for j in range(1, N + 1):
+        if V[i][j] or M[i][j] != K:
+            continue
+        Q = deque()
+        V[i][j] = 1
+        cnt = 0
+        Q.append([i, j])
+        while Q:
+            cy, cx = Q.popleft()
+            cnt += 1
+            for k in range(4):
+                ny, nx = cy + dy[k], cx + dx[k]
+                if ny < 1 or nx < 1 or ny > N or nx > N:
+                    continue
+                if V[ny][nx] or M[ny][nx] != M[cy][cx]:
+                    continue
+                V[ny][nx] = 1
+                Q.append([ny, nx])
+        ans = max(ans, cnt)
+
+print(ans)
