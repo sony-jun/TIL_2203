0c676d33178caa19c7edfa91c636b03a09d1e386
sony-jun <warrise@naver.com>
2023-07-14 00:04:26 +0900

    7.14

--- Algorithm/test.py
+++ Algorithm/test.py
@@ -1,3 +1,13 @@
-a = 10
+from collections import Counter
 
-print(f"(a)" " is even")
+
+def solution(participant, completion):
+    answer = ""
+    p_dict = Counter(participant)
+    c_dict = Counter(completion)
+
+    for p_name in p_dict:
+        if p_dict[p_name] != c_dict[p_name]:
+            answer = p_name
+
+    return answer
