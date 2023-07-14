f880102531ce6ff8854d2d9ebd59f4545ff9da22
sony-jun <warrise@naver.com>
2023-07-15 02:23:29 +0900

    7.15

--- Algorithm/test.py
+++ Algorithm/test.py
@@ -1,13 +1,15 @@
-from collections import Counter
-
-
-def solution(participant, completion):
-    answer = ""
-    p_dict = Counter(participant)
-    c_dict = Counter(completion)
-
-    for p_name in p_dict:
-        if p_dict[p_name] != c_dict[p_name]:
-            answer = p_name
-
-    return answer
+ineq, eq, n, m = input().split()
+answer = 0
+if ineq == "<":
+    if n < m:
+        answer = 1
+elif ineq == "<" and eq == "=":
+    if n <= m:
+        answer = 1
+elif ineq == ">":
+    if n > m:
+        answer = 1
+elif ineq == ">" and eq == "=":
+    if n >= m:
+        answer = 1
+print(answer)
