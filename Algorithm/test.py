bc2be01a87cac874b0c977690267b1ab006f2074
sony-jun <warrise@naver.com>
2023-07-18 00:05:11 +0900

    7.18

--- Algorithm/test.py
+++ Algorithm/test.py
@@ -1,15 +1,11 @@
-ineq, eq, n, m = input().split()
+num_list = map(int, input().split())
 answer = 0
-if ineq == "<":
-    if n < m:
-        answer = 1
-elif ineq == "<" and eq == "=":
-    if n <= m:
-        answer = 1
-elif ineq == ">":
-    if n > m:
-        answer = 1
-elif ineq == ">" and eq == "=":
-    if n >= m:
-        answer = 1
+a = 1
+b = 0
+for i in num_list:
+    a *= i
+    b += i
+c = b * b
+if a < b:
+    answer = 1
 print(answer)
