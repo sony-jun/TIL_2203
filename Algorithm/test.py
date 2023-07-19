637d81d92f8e2d11143bae5cc4c30148ce93b6b1
sony-jun <warrise@naver.com>
2023-07-20 00:02:53 +0900

    7.20

--- Algorithm/test.py
+++ Algorithm/test.py
@@ -1,11 +1,26 @@
-num_list = map(int, input().split())
-answer = 0
-a = 1
-b = 0
-for i in num_list:
-    a *= i
-    b += i
-c = b * b
-if a < b:
-    answer = 1
+genres = input().split()
+plays = map(int, input().split())
+answer = []
+music_play = dict()
+
+# 장르별로 play 횟수
+for k, v in zip(genres, plays):
+    music_play[k] = music_play.get(k, 0) + int(v)
+
+    # play 횟수 내림차순으로 정렬
+play_list = sorted(music_play.items(), key=lambda x: x[1], reverse=True)
+
+# 많이 재생된 장르 순으로 for문 돌리기
+for l, cnt in play_list:
+    arr = []
+    # 재생횟수와 고유번호 넣어주기
+    for idx, (g, p) in enumerate(zip(genres, plays)):
+        if g == l:
+            arr.append([p, idx])
+
+        # 조건 2,3 을 고려한 정렬
+    arr = sorted(arr, key=lambda x: (x[0], -x[1]), reverse=True)
+
+    for i in range(min(len(arr), 2)):
+        answer.append(arr[i][1])
 print(answer)
