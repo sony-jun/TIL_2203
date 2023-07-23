659c2c2caf0a2c8d1dec1582da911bd439a17c82
sony-jun <warrise@naver.com>
2023-07-24 00:39:49 +0900

    7.24

--- Algorithm/test.py
+++ Algorithm/test.py
@@ -1,6 +1,3 @@
-a = ["0123456789", "9876543210", "9999999999999"]
-answer = []
-for i in a:
-    if int(i[5:11]) > 50000:
-        answer.append(int(i[5:10]))
-print(answer)
+bridge = [0 for _ in range(100)]
+
+print(bridge)
